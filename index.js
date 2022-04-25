ddffec3aa8904f3e9593b71429dfd60f268ea674
Hermes Júnior <hermesj.junior@gmail.com>
2022-04-25 16:34:59 -0300

    Criação do segundo endpoint

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
-    if(user.username === "" || user.userimage === "") {
+    if(user.username === "" || user.avatar === "") {
         return res.status(400).send("Preencher todos os campos!");
     }
     users.push(user);
     res.status(201).send("Usuário cadastrado!");
+});
+
+app.post("/tweets", (req, res) => {
+    let username = req.header("User");
+    if(!username) {
+        username = users[users.length - 1].username;
+    }
+    const {avatar} = users[users.length - 1];
+    const {tweet} = req.body;
+    if(username === "" || tweet === "") {
+        return res.status(400).send("Preencher todos os campos!");
+    }
+    tweets.push({
+        username,
+        avatar,
+        tweet
+    });
+    res.status(201).send("Tweet criado!");
 })
