4eec07adcc45a9dfc93bd0e1b0615303a4944ed5
Hermes Júnior <hermesj.junior@gmail.com>
2022-04-25 16:12:59 -0300

    Criação do primeiro endpoint

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ import express, { json } from 'express';
 
 const app = express();
 app.use(json());
+app.use(cors());
+
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body;
+    if(user.username === "" || user.userimage === "") {
+        return res.status(400).send("Preencher todos os campos!");
+    }
+    users.push(user);
+    res.status(201).send("Usuário cadastrado!");
+})
